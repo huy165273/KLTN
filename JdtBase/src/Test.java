fbda8ae3e16a23e3e5f839a9a1ad5e050f944040
ngochuy2207 <nngochuy2207@gmail.com>
2025-03-03 01:28:13 +0700

    fix file path

--- JdtBase/src/Test.java
+++ JdtBase/src/Test.java
@@ -1,49 +1,16 @@
-import java.io.*;
 public class Test {
-public static boolean mark(String append) {
-   try {
-       FileWriter fileWriter = new FileWriter(TEST_PATH, true);
-       BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+    public static char grade(int averageGrade) {
+        if (averageGrade > 90 && averageGrade < 100)
+            return 'A';
+        else if (averageGrade > 80 && averageGrade < 90)
+            return 'B';
+        else if (averageGrade > 70 && averageGrade < 80)
+            return 'C';
+        else if (averageGrade > 60 && averageGrade < 70)
+            return 'D';
+        else if (averageGrade > 0 && averageGrade < 60)
+            return 'F';
+        return 'I'; // 'I' for invalid input
+    }
+}
 
-       bufferedWriter.write(append + '\n');
-
-       bufferedWriter.close();
-       fileWriter.close();
-   } catch (IOException e) {
-   e.printStackTrace();
-  }
-   return true;
-}
-public static char grade(int averageGrade){
-  if ((mark("id$37.0#line-in-function$2#offset$50#condition$falseNode#statement$averageGrade > 90") && averageGrade > 90 && mark("id$37.0#line-in-function$2#offset$50#condition$trueNode#statement$averageGrade > 90")) && (mark("id$38.0#line-in-function$2#offset$71#condition$falseNode#statement$averageGrade < 100") && averageGrade < 100 && mark("id$38.0#line-in-function$2#offset$71#condition$trueNode#statement$averageGrade < 100"))) {
-    mark("id$9.0#line-in-function$3#offset$97#statement$return 'A';");return 'A';
-  }
- else   if ((mark("id$39.0#line-in-function$5#offset$125#condition$falseNode#statement$averageGrade > 80") && averageGrade > 80 && mark("id$39.0#line-in-function$5#offset$125#condition$trueNode#statement$averageGrade > 80")) && (mark("id$40.0#line-in-function$5#offset$146#condition$falseNode#statement$averageGrade < 90") && averageGrade < 90 && mark("id$40.0#line-in-function$5#offset$146#condition$trueNode#statement$averageGrade < 90"))) {
-    mark("id$15.0#line-in-function$6#offset$171#statement$return 'B';");return 'B';
-  }
- else   if ((mark("id$41.0#line-in-function$8#offset$199#condition$falseNode#statement$averageGrade > 70") && averageGrade > 70 && mark("id$41.0#line-in-function$8#offset$199#condition$trueNode#statement$averageGrade > 70")) && (mark("id$42.0#line-in-function$8#offset$220#condition$falseNode#statement$averageGrade < 80") && averageGrade < 80 && mark("id$42.0#line-in-function$8#offset$220#condition$trueNode#statement$averageGrade < 80"))) {
-    mark("id$21.0#line-in-function$9#offset$245#statement$return 'C';");return 'C';
-  }
- else   if ((mark("id$43.0#line-in-function$11#offset$273#condition$falseNode#statement$averageGrade > 60") && averageGrade > 60 && mark("id$43.0#line-in-function$11#offset$273#condition$trueNode#statement$averageGrade > 60")) && (mark("id$44.0#line-in-function$11#offset$294#condition$falseNode#statement$averageGrade < 70") && averageGrade < 70 && mark("id$44.0#line-in-function$11#offset$294#condition$trueNode#statement$averageGrade < 70"))) {
-    mark("id$27.0#line-in-function$12#offset$319#statement$return 'D';");return 'D';
-  }
- else   if ((mark("id$45.0#line-in-function$14#offset$347#condition$falseNode#statement$averageGrade > 0") && averageGrade > 0 && mark("id$45.0#line-in-function$14#offset$347#condition$trueNode#statement$averageGrade > 0")) && (mark("id$46.0#line-in-function$14#offset$367#condition$falseNode#statement$averageGrade < 60") && averageGrade < 60 && mark("id$46.0#line-in-function$14#offset$367#condition$trueNode#statement$averageGrade < 60"))) {
-    mark("id$33.0#line-in-function$15#offset$392#statement$return 'F';");return 'F';
-  }
-  mark("id$35.0#line-in-function$17#offset$410#statement$return 'I';");return 'I';
-}
-public static String TEST_PATH = "D:/Code/KLTN/KLTN/result/marks/gradeMarks/TestDriverResult1.txt";
-public static void main(String[] args) {
-   File file;
-   //TODO: Call test function
-   {
-        file = new File(TEST_PATH);
-        if (file.exists()) {
-            file.delete();
-        }
-        int averageGrade;
-        averageGrade= 5;
-        grade(averageGrade);
-   }
-}
-}
